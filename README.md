b9c4c142de5ef6fb7c1ab19b0879bb427f3b6db4
Nitin Sachdeo <nitinsachdeo@gmail.com>
2021-04-11 18:04:38 -0400

    Initialize repo

---
+++ README.md
@@ -0,0 +1,6 @@
+Human Benchmark Bot
+===================
+
+This repository consists of a browser automation based python bot intended to be the
+best there ever was on all the tests part of the
+[Human Benchmark](https://humanbenchmark.com/).
---
+++ setup.py
@@ -0,0 +1,26 @@
+#!/usr/bin/env python
+"""The setup script"""
+
+from setuptools import setup, find_packages
+
+with open("README.md") as readme_file:
+    readme = readme_file.read()
+
+requirements = []
+
+setup(
+    author="Nitin Sachdeo",
+    author_email="nitinsachdeo@gmail.com",
+    python_requires=">=3.9",
+    description="A browser automation based bot intended to be the best there ever was on all the tests part of the Human Benchmark.",
+    install_requires=requirements,
+    license="MIT license",
+    long_description=readme,
+    include_package_data=True,
+    keywords="humanbenchmark",
+    name="humanbenchmarkbot",
+    packages=find_packages(include=["humanbenchmark", "humanbenchmark.*"]),
+    url="https://github.com/NitinSachdeo/HumanBenchmarkBot",
+    version="0.1.0",
+    zip_safe=False,
+)
