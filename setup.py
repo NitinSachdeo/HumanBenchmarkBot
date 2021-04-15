b3c764407e07278877f227e1a6a432179f279a2e
Nitin Sachdeo <nitinsachdeo@gmail.com>
2021-04-15 11:00:33 -0400

    Update package requirements

--- setup.py
+++ setup.py
@@ -6,7 +6,7 @@ from setuptools import setup, find_packages
 with open("README.md") as readme_file:
     readme = readme_file.read()
 
-requirements = []
+requirements = ["keyboard", "rich", "selenium", "webdriver_manager"]
 
 setup(
     author="Nitin Sachdeo",
